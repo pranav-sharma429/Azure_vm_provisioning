eb6560b8f66a56266da235dce209944d93eca078
Pranav Sharma <pranav.sharma1799@gmail.com>
2022-03-22 19:17:45 -0400

    Multiple Changes, this copy has provisioning & tomcat

--- roles/redhat/tasks/gather.sh
+++ roles/redhat/tasks/gather.sh
@@ -3,4 +3,4 @@
 pubip=`az vm show -d -g devresgroup -n devvm001 --query publicIps -o tsv` 
 
 echo -e "[devvm001]  \n$pubip" >> /etc/ansible/hosts
-
+echo -e "devvm001: $pubip" >> /home/Azure/group_vars/all.yml
